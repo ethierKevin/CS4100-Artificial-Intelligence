46d6e36e4b01c510459e673ca06419fe9040fa85
Ethier <Kevin.Ethier@emc.com>
2016-02-12 11:44:21 -0500

    DFS_Edit

--- PacMan_v6.2/src/pacman/controllers/DFS_Controller_Kevin_Ethier.java
+++ PacMan_v6.2/src/pacman/controllers/DFS_Controller_Kevin_Ethier.java
@@ -21,11 +21,11 @@ import pacman.game.Game;
 /**
  * @author Kevin M. Ethier
 
-UP 	{ public MOVE opposite(){return MOVE.DOWN;	};},	
-RIGHT 	{ public MOVE opposite(){return MOVE.LEFT;	};}, 	
-DOWN 	{ public MOVE opposite(){return MOVE.UP;		};},		
-LEFT 	{ public MOVE opposite(){return MOVE.RIGHT;	};}, 	
-NEUTRAL	{ public MOVE opposite(){return MOVE.NEUTRAL;	};};
+UP  { public MOVE opposite(){return MOVE.DOWN;  };},    
+RIGHT   { public MOVE opposite(){return MOVE.LEFT;  };},    
+DOWN    { public MOVE opposite(){return MOVE.UP;        };},        
+LEFT    { public MOVE opposite(){return MOVE.RIGHT; };},    
+NEUTRAL { public MOVE opposite(){return MOVE.NEUTRAL;   };};
 */
 public class DFS_Controller_Kevin_Ethier extends Controller<MOVE> {
             
@@ -40,27 +40,29 @@ public class DFS_Controller_Kevin_Ethier extends Controller<MOVE> {
     }
 
     public static StarterGhosts ghosts = new StarterGhosts();
-    
-
-    int bestScore = -1;
-    MOVE bestMove = MOVE.LEFT;
 
     @Override
     public MOVE getMove(Game game, long timeDue) {
+
         List<MOVE> moves = Arrays.asList(MOVE.DOWN, MOVE.LEFT, MOVE.UP, MOVE.RIGHT, MOVE.NEUTRAL);
+        int bestScore = -1;
+        MOVE bestMove = MOVE.LEFT;
 
         // For all possible moves create a game object then copy it.  Run the first 
         // move as a simulation and save in the copy, pr
         for (MOVE m : moves) {
-            Game whatIfGame = game.copy(); 
-            CurrentGameState freezeGame = new CurrentGameState(whatIfGame,7);
-            whatIfGame.advanceGame(m, ghosts.getMove(game, timeDue));    
+            Game whatIfGame = game.copy();
+
+
+            whatIfGame.advanceGame(m, ghosts.getMove(whatIfGame, timeDue));
             
             // Compare highest score to current best score and from there determine
             // the best move
             System.out.println(bestMove.toString());
-            if (this.dfs_kevin(freezeGame) > bestScore) {                
-                bestScore = dfs_kevin(freezeGame);
+
+            CurrentGameState freezeGame = new CurrentGameState(whatIfGame,0);
+            if (this.dfs_kevin(freezeGame,7) > bestScore) {
+                bestScore = dfs_kevin(freezeGame,7);
                 bestMove = m;
             }
         }
@@ -68,9 +70,9 @@ public class DFS_Controller_Kevin_Ethier extends Controller<MOVE> {
     }
     
     // My implementation of Depth First Search 
-    public int dfs_kevin(CurrentGameState current){
+    public int dfs_kevin(CurrentGameState current, int maxDepth){
         
-        int highestScore = 0; 
+        int highestScore = -1;
         List<MOVE> moves = Arrays.asList(MOVE.DOWN, MOVE.LEFT, MOVE.UP, MOVE.RIGHT, MOVE.NEUTRAL);
         
         // Create a Stack and push the current game state onto it 
@@ -83,14 +85,14 @@ public class DFS_Controller_Kevin_Ethier extends Controller<MOVE> {
         // to new CurrentGameState object
         while(!S.isEmpty()){ 
             
-            System.out.println("inside the while loop" + i); 
+            System.out.println("inside the while loop --> " + i + "times !!");
             i++;
             
             CurrentGameState thisGame = S.pop(); 
             // IF the depth is max that means you've reached the determined depth 
             // and this will be the top score for this path so compare it to the 
             // other scores.
-            if(thisGame.depth >= current.depth && thisGame.current.getScore() >= highestScore)
+            if(thisGame.depth >= maxDepth && thisGame.current.getScore() >= highestScore)
                 highestScore = thisGame.current.getScore();
             
             // ELSE this isn't the case then there are more children and they
